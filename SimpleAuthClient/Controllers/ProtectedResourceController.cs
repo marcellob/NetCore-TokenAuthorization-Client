096f21dcfa62e678a162e4f5f616803be507a255
mbelguar <mbelguar@IEP7L4KD8K32>
2018-10-12 09:57:52 +0100

    Add project files.

---
+++ SimpleAuthClient/Controllers/ProtectedResourceController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace SimpleAuthClient.Controllers
+{
+	[Authorize]
+	[Route("api/ProtectedResource")]
+	[ApiController]
+	public class ProtectedResourceController : ControllerBase
+	{
+
+		[Authorize(Roles = "DesktopAdmin1")]
+		[HttpGet("Endpoint1")]
+		public dynamic GetEndpoint1()
+		{
+			return DisplayDetails();
+		}
+
+		[Authorize(Roles = "DesktopAdmin2")]
+		[HttpGet("Endpoint2")]
+		public dynamic GetEndpoint2()
+		{
+			return DisplayDetails();
+		}
+
+		[Authorize(Roles = "DesktopAdmin3")]
+		[HttpGet("Endpoint3")]
+		public dynamic GetEndpoint3()
+		{
+			return DisplayDetails();
+		}
+
+		// Roles authorized in "Or"
+		[Authorize(Roles = "DesktopAdmin1, DesktopAdmin2")]
+		[HttpGet("Endpoint4")]
+		public dynamic GetEndpoint4()
+		{
+			return DisplayDetails();
+		}
+
+		// Roles authorized in "And"
+		[Authorize(Roles = "DesktopAdmin1")]
+		[Authorize(Roles = "DesktopAdmin2")]
+		[HttpGet("Endpoint5")]
+		public dynamic GetEndpoint5()
+		{
+			return DisplayDetails();
+		}
+
+		[HttpGet("Authentication/Me")]
+		public dynamic Me()
+		{
+			return DisplayDetails();
+		}
+
+		private dynamic DisplayDetails() => new
+		{
+			Name = User.Identity.Name,
+			Surname = User.Claims.Where(c => c.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname").FirstOrDefault().Value,
+			LanId = User.Claims.Where(c => c.Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/windowsaccountname").FirstOrDefault().Value,
+			Roles = User.Claims.Where(c => c.Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role").Select(r => r.Value).ToList(),
+			IsAuthenticated = User.Identity.IsAuthenticated,
+		};
+	}
+}
---
+++ SimpleAuthClient/Startup.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace SimpleAuthClient
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+
+			services.AddCors();
+			services.AddMvc();
+
+			// configure strongly typed settings objects
+			var appSettingsSection = Configuration.GetSection("AppSettings");
+			services.Configure<AppSettings>(appSettingsSection);
+
+			// configure jwt authentication
+			var appSettings = appSettingsSection.Get<AppSettings>();
+			var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+			services.AddAuthentication(x =>
+			{
+				x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+				x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+			})
+			.AddJwtBearer(x =>
+			{
+				x.RequireHttpsMetadata = false;
+				x.SaveToken = true;
+				x.TokenValidationParameters = new TokenValidationParameters
+				{
+				
+					IssuerSigningKey = new SymmetricSecurityKey(key),
+					ValidIssuer = "simpleauthserver.aig.com",
+					ValidAudience = "simpleauthclient.aig.com",
+					ValidateIssuerSigningKey = true,
+					ValidateIssuer = true,
+					ValidateAudience = true,
+					ValidateLifetime = true
+					
+				};
+			});
+
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+			loggerFactory.AddDebug();
+
+			// global cors policy
+			app.UseCors(x => x
+				.AllowAnyOrigin()
+				.AllowAnyMethod()
+				.AllowAnyHeader()
+				.AllowCredentials());
+
+			app.UseAuthentication();
+
+			app.UseMvc();
+		}
+
+
+	}
+}
